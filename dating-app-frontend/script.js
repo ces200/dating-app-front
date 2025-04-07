ea091be96f527fdae099c35fa36859aba4fb8038
ces200 <ces200@naver.com>
2025-04-07 20:44:38 +0900

    Add files via upload

---
+++ dating-app-frontend/script.js
@@ -0,0 +1,8 @@
+document.getElementById('registerPage').addEventListener('click', () => {
+    document.getElementById('content').innerHTML = '<h1>등록 페이지</h1>';
+  });
+  
+  document.getElementById('viewPage').addEventListener('click', () => {
+    document.getElementById('content').innerHTML = '<h1>조회 페이지</h1>';
+  });
+  
